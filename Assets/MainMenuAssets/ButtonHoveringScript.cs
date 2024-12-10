4604c7ff93cf2fe04d8e49e4aec2552024805e9b
6x39 <brooklyn.gooding1@gmail.com>
2024-12-10 22:09:47 +0000

    trying to work on the hovertext a bit more, not good

--- Assets/MainMenuAssets/ButtonHoveringScript.cs
+++ Assets/MainMenuAssets/ButtonHoveringScript.cs
@@ -8,17 +8,23 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class ButtonHoveringScript : MonoBehaviour, IPointerExitHandler
+public class ButtonHoveringScript : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 {
-    public TMP_Text buttonText;
+    public TextMeshProUGUI buttonText;
     public void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("The cursor entered the selectable UI element.");
-        buttonText.fontSize += 10;
+        buttonText.fontSize = Mathf.Lerp(64f, 70f, Time.deltaTime * 0.5f);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("The cursor exited the selectable UI element.");
-        buttonText.fontSize -= 10;
+        if (buttonText.fontSize > 64f) 
+        {
+            buttonText.fontSize = Mathf.Lerp(70f, 64f, Time.deltaTime * 0.5f);
+        }
     }
 }
+// will come to this later, as I'm not sure why it's not working.
+// The TextMeshProUGUI buttonText object isn't being classed as an object for some reason, so it's preventing my code from working properly.
+// Additionally, whenever the pointer enters the button, nothing actually happens.
