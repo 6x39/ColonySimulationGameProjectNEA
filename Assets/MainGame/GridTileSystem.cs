1977ec7b9fc867efb7cd616dbc4425faf856bb6a
6x39 <brooklyn.gooding1@gmail.com>
2025-01-09 14:43:12 +0000

    Grids, working on digging.
    I have started the first parts of colliders for my tiles, as well as digging. Digging is not functional yet, but is getting there.

--- Assets/MainGame/GridTileSystem.cs
+++ Assets/MainGame/GridTileSystem.cs
@@ -6,14 +6,14 @@ using Unity.Mathematics;
 using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 // this current system means that whenever the game stops running, all of the objects will be destroyed too. therefore, this is not a permanent solution. I will need to implement a saving system that logs the position of each block and "saves" them more or less.
 public class GridTileSystem : MonoBehaviour
 {
     private int objectWidth; // number of blocks wide
     private int objectHeight; // number of blocks tall
-    private float gapSize; // the gap between each block.
-    GameObject prefabTile = (GameObject)Resources.Load("initialPrefabTile");// this is loading the prototype tile that is going to be used for the entire grid.
+    private float gapSize; // the gap between each block
 
     public GridTileSystem(int width, int height, float gapSize)
     {
@@ -24,14 +24,36 @@ public class GridTileSystem : MonoBehaviour
     
     public void GenerateGrid()
     {
+        GameObject prefabTile = (GameObject)Resources.Load("initialPrefabTile");
+        GameObject TileHolder = GameObject.Find("TileHolder");
         for (int x = 0; x < objectWidth; x++)
         {
             for (int y = 0; y < objectHeight; y++)
             {
                 Vector3 position = new Vector3((x * gapSize) + 0.5f, (y * gapSize) + 0.5f, 0);
-                GameObject tile = Instantiate(prefabTile, position, quaternion.identity);   
+                GameObject tile = Instantiate(prefabTile, position, Quaternion.identity);   
                 tile.name = $"tile_{x}_{y}";
+                tile.transform.parent = TileHolder.transform;
+                tile.AddComponent(typeof(SphereCollider));
             }
         }
     }
+    
+    public void DigFunction() // this will be repurposed at a later day for my mechanics.
+    {
+        Vector3 cursorPosition = Input.mousePosition;
+        Collider[] intersecting = Physics.OverlapSphere(cursorPosition, 0.01f);
+        if (intersecting.Length != 0)
+        {
+            foreach (Collider thingToDestroy in intersecting)
+            {
+                float dist = Vector3.Distance(cursorPosition, thingToDestroy.transform.position);
+                if (dist < 0.5f)
+                {
+                    Destroy(thingToDestroy);
+                }
+            }
+        }
+    }
+    
 }
--- Assets/MainGame/TestingGridMaker.cs
+++ Assets/MainGame/TestingGridMaker.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class TestingGridMaker : MonoBehaviour
 {
     // Start is called before the first frame update
+    GridTileSystem gridGenerator = new GridTileSystem(10, 5, 1f);
     void Start()
     {
-        GridTileSystem gridGenerator = new GridTileSystem(10, 5, 1f);
         gridGenerator.GenerateGrid();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gridGenerator.DigFunction();
+        }
     }
 }
